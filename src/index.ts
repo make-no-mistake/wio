f6439472822961f98bac7addc5ae91c6c8a10980
ch-iv <icheppy101@gmail.com>
2026-01-17 17:27:41 -0500

    Clean-up unused routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Fastify from "fastify";
-import { sql } from "bun";
 import { appAndSiteSpaceSwitch } from "./callbacks/appAndSiteSpaceSwitch";
 import { retrieveSiteAsset } from "./site/assets";
 import { initDatabase } from "./db/schema";
@@ -14,25 +13,9 @@ const fastify = Fastify({
 await initDatabase();
 await initStorage();
 
-fastify.get("/", async (request, reply) => {
+fastify.get("/", async (_, reply) => {
   const index_content = await readFile(import.meta.dir + "/static/index.html");
   reply.header("Content-Type", "text/html").send(index_content);
-});
-
-fastify.get("/time", async () => {
-  const result = await sql`SELECT now() AS time`;
-  return result[0];
-});
-
-fastify.get("/all", async () => {
-  const sites = await sql`SELECT * FROM site_assets`;
-  return sites;
-});
-
-fastify.get("/sites/:site/identity", async (request, reply) => {
-  const { site } = request.params as { site: string };
-
-  return reply.send(`You are on a site "${site}"`);
 });
 
 fastify.get("/sites/:site/:asset", async (request, reply) => {
@@ -40,10 +23,6 @@ fastify.get("/sites/:site/:asset", async (request, reply) => {
 
   const { bytes, mimetype } = await retrieveSiteAsset(site, asset);
   return reply.header("Content-Type", mimetype).send(bytes);
-});
-
-fastify.get("/identity", (_, reply) => {
-  return reply.send("You are on the root URL");
 });
 
 fastify.listen({ port: 3000, host: "0.0.0.0" }, (err) => {
