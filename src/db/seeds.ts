fb4af8ca8a195204723de341995cc21a704aab63
Yianni Culmone <154041314+CulmoneY@users.noreply.github.com>
2026-02-10 17:41:10 -0500

    [DB] Modified Seeds To Correct Use Site Files Over Site Assets (#57)

--- src/db/seeds.ts
+++ src/db/seeds.ts
@@ -1,14 +1,27 @@
 import { sql } from "bun";
 
 export async function seed() {
-  await sql`
-      DELETE FROM site_assets`;
+  const users = [{ unique_id: "example-user" }];
+
+  const [user] = await sql<{ id: number; unique_id: string }[]>`
+    INSERT INTO users ${sql(users)}
+    RETURNING id, unique_id;`;
+
+  const sites = [{ name: `${user!.id}_mysite`, owner_id: user!.id }];
+
+  const [site] = await sql<{ name: string; id: number }[]>`
+    INSERT INTO sites ${sql(sites)}
+    RETURNING name, id;`;
+
+  const siteFiles = [
+    {
+      site_id: site!.id,
+      s3_path: `${site!.name}/cat.png`,
+      file_name: "cat",
+      mimetype: "image/png",
+    },
+  ];
 
   await sql`
-      INSERT INTO site_assets (site_id, filename)
-      VALUES ('example', 'example.txt')`;
-
-  await sql`
-      INSERT INTO site_assets (site_id, filename)
-      VALUES ('example', 'cat.png')`;
+    INSERT INTO site_files ${sql(siteFiles)};`;
 }
